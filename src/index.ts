3421f4b3eab600e27187582142ecb9f6bd72b619
Andrei <andrei.ionescu3535@gmail.com>
2026-07-25 21:32:19 +0300

    add CLIENT_URL

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const run = async () => {
   mongoose.connect(process.env.MONGO_URI as string);
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: process.env.CLIENT_URL,
       credentials: true,
     }),
   );
